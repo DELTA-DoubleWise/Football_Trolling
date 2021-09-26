caa759cf52387bfa4d8f3995825001f6b8d16480
DELTA-DoubleWise <deltawang7@gmail.com>
2021-09-26 13:54:50 +0800

    reformat the data

--- src/crawler.py
+++ src/crawler.py
@@ -8,16 +8,9 @@ from psaw import PushshiftAPI
 from tqdm import tqdm
 import pickle
 from utils import crawler_utils
-from utils.crawler_utils import time_initialization
+from utils.crawler_utils import time_initialization, club_abbr, club_reddit_abbr
 
 api = PushshiftAPI()
-
-club_reddit_abbr = ['ACMilan', 'atletico', 'Barca', 'borussiadortmund', 'chelseafc', 'fcbayern', 'FCInterMilan', 'Gunners',
-                    'Juve', 'LiverpoolFC', 'MCFC', 'psg', 'realmadrid', 'reddevils', 'roma', 'Tottenham']
-
-club_abbr = ['MIL', 'AMD', 'BAR', 'DOR', 'CHE', 'MUN', 'INT', 'ARS',
-             'JUV', 'LIV', 'MNC', 'PSG', 'RMD', 'MNU', 'ROM', 'TOT']
-
 
 start_time, end_time = time_initialization()
 
@@ -25,11 +18,10 @@ for club_index in tqdm(range(len(club_abbr))):
     temp_list = []
     for i in tqdm(range(len(start_time))):
         temp_list.append(np.array(list(api.search_submissions(after=start_time[i], before=end_time[i],
-                                                                subreddit=club_reddit_abbr[club_index],
-                                                                filter=['url', 'author', 'title', 'subreddit', 'id',
-                                                                        'num_comments', 'score', 'ups', 'upvote_ratio'])
-                                                    )))
-    # temp_array = np.array(temp_list, dtype=object)
+                                                              subreddit=club_reddit_abbr[club_index],
+                                                              filter=['url', 'author', 'title', 'subreddit', 'id',
+                                                                      'num_comments', 'score', 'ups', 'upvote_ratio'])
+                                       )))
     file = open(f'./data/{club_abbr[club_index]}.pickle', 'wb')
     pickle.dump(temp_list, file)
     file.close()
--- src/utils/crawler_utils.py
+++ src/utils/crawler_utils.py
@@ -1,25 +1,36 @@
 import praw
 import pandas as pd
+import numpy as np
 import requests
 from praw.models import MoreComments
 import datetime as dt
 from psaw import PushshiftAPI
 from tqdm import tqdm
+import pickle
+
+club_reddit_abbr = ['ACMilan', 'atletico', 'Barca', 'borussiadortmund', 'chelseafc', 'fcbayern', 'FCInterMilan',
+                    'Gunners',
+                    'Juve', 'LiverpoolFC', 'MCFC', 'psg', 'realmadrid', 'reddevils', 'roma', 'Tottenham']
+
+club_abbr = ['MIL', 'AMD', 'BAR', 'DOR', 'CHE', 'MUN', 'INT', 'ARS',
+             'JUV', 'LIV', 'MNC', 'PSG', 'RMD', 'MNU', 'ROM', 'TOT']
 
 
 # reddit api initialization
 def api_initialization():
-    auth = requests.auth.HTTPBasicAuth('bCnE1U61Wqixgs2wy28POg','vEY7k3_j7o3PZZvP-tEt6DnhWr1x5A')
+    auth = requests.auth.HTTPBasicAuth('bCnE1U61Wqixgs2wy28POg', 'vEY7k3_j7o3PZZvP-tEt6DnhWr1x5A')
 
     data = {'grant_type': 'password',
-        'username': 'Delta_Wang11',
-        'password': 'delta113420'
-        }
+            'username': 'Delta_Wang11',
+            'password': 'delta113420'
+            }
 
-    headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/93.0.4577.82 Safari/537.36'}
 
     res = requests.post('https://www.reddit.com/api/v1/access_token',
-                     auth=auth, data=data, headers=headers)
+                        auth=auth, data=data, headers=headers)
 
     TOKEN = res.json()['access_token']
     headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
@@ -38,6 +49,32 @@ def time_initialization():
         end_time_list.append(int(dt.datetime(year[i], month[i], end_day[i]).timestamp()))
 
     return start_time_list, end_time_list
+
+
+def list_flatten(target):
+    return [item for sublist in target for item in sublist]
+
+
+def transform_df(filename):
+    with open(filename, 'rb') as file:
+        posts = pickle.load(file)
+    posts = list_flatten(posts)
+    posts = np.array(posts)
+    posts_info = posts[:, 10]
+    df = pd.DataFrame()
+    for post in posts_info:
+        df = df.append({
+            'author': post['author'],
+            'id': post['id'],
+            'created_utc': post['created_utc'],
+            'subreddit': post['subreddit'],
+            'url': post['url'],
+            'title': post['title'],
+            'num_comments': post['num_comments'],
+            'score': post['score'],
+            'upvote_ratio': post['upvote_ratio']
+        }, ignore_index=True)
+    return df
 
 
 
